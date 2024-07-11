a5a097159b79e51c9d0c55f173316425068f4bb7
MosesOnerhime <mosesonerhime11@gmail.com>
2024-07-11 16:59:58 +0100

    commit

--- Week-1/Day-4-Thursday/calculator/target/debug/calculator.d
+++
@@ -1,1 +0,0 @@
-C:\Users\moses\source\repos\Convexity\ Technologies\ Internship\Week-1\Day-4-Thursday\calculator\target\debug\calculator.exe: C:\Users\moses\source\repos\Convexity\ Technologies\ Internship\Week-1\Day-4-Thursday\calculator\src\main.rs
