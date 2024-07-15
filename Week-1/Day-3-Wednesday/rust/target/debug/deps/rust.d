4e57acf1344f7b57d6281052f6cec0888d195780
MosesOnerhime <mosesonerhime11@gmail.com>
2024-07-15 09:42:51 +0100

    commit

--- Week-1/Day-3-Wednesday/rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\moses\source\repos\Convexity Technologies Internship\Week-1-Day-3\rust\target\debug\deps\rust.exe: src/main.rs
-
-C:\Users\moses\source\repos\Convexity Technologies Internship\Week-1-Day-3\rust\target\debug\deps\rust.d: src/main.rs
-
-src/main.rs:
--- Week-1/Day-3-Wednesday/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\moses\source\repos\Convexity\ Technologies\ Internship\Week-1-Day-3\rust\target\debug\rust.exe: C:\Users\moses\source\repos\Convexity\ Technologies\ Internship\Week-1-Day-3\rust\src\main.rs
