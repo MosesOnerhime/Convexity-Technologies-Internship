23088bb678db5167e8f098728b14bd718b529294
MosesOnerhime <mosesonerhime11@gmail.com>
2024-07-29 16:38:52 +0100

    commit

--- Week-1/Day-3-Wednesday/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
+++ Week-1/Day-3-Wednesday/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd07f900d352955562561d9f3fe42a1b7b4a071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81abf6b4540aa674a09d683d1239901a1ca33717")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
